df64dec0b52a1bd559bbba24bb72b1b6a1b144da
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-11 16:25:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
+
+    require "pry"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+#binding.pry
 def key_for_min_value(name_hash)
+  
+lowest_key = nil
+lowest_value = nil
+name_hash.each do |key,value|
+if lowest_key ==nil || value <lowest_value
+  #binding.pry
+   lowest_value = value
+   lowest_key = key 
+ end
+ end
+ 
+ lowest_key
 
 end
